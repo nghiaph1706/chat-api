f991306c62d37cfd9deee1ae6b045ade5f853dbe
nghia <nghiaplps17855@fpt.edu.vn>
2022-08-14 02:15:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 require('dotenv/config')
 
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 app.use(cors())
 app.use(bodyParser.json())
 
